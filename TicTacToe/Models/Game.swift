988b238d0f93f6f375c4d1103f50f2ffc43d4433
makkina <efe@appamist.com>
2021-07-11 14:23:30 +0200

    test_randomPlayerCanWin_givenTheFirstWinningDiagonalCombinationWasFound

--- TicTacToe/Models/Game.swift
+++ TicTacToe/Models/Game.swift
@@ -25,7 +25,9 @@ struct Game {
         // Horizontal
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         // Vertical
-        [0, 3, 6], [1, 4, 7], [2, 5, 8]
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        // Diagonal
+        [0, 4, 8]
     ]
     
     init(activePlayer: Player, selectedBoardValues: [Int], gameState: GameState, winner: Player? = nil) {
