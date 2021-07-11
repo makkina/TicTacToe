1c0bf5830ea48eff59df85d6c6991958e600ba0b
makkina <efe@appamist.com>
2021-07-11 21:44:43 +0200

    Remove force unwrapp from restartGameButtonisHidden

--- TicTacToe/ViewModels/BoardViewModel.swift
+++ TicTacToe/ViewModels/BoardViewModel.swift
@@ -5,7 +5,7 @@ import Foundation
 final class BoardViewModel {
     
     private (set) var game: GameProtocol!
-    private (set) var restartGameButtonIsHidden: Bool! = true
+    private (set) var restartGameButtonIsHidden: Bool = true
     private (set) var gameLabelText: String = "Player X turn"
 
     init(game: GameProtocol) {
