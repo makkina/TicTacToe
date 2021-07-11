6566447aa9d2cc06ec2bce9101f0a098783fe376
makkina <efe@appamist.com>
2021-07-11 21:44:16 +0200

    test_setsGameLabelTextToPlayerXTurn

--- TicTacToe/ViewModels/BoardViewModel.swift
+++ TicTacToe/ViewModels/BoardViewModel.swift
@@ -6,7 +6,8 @@ final class BoardViewModel {
     
     private (set) var game: GameProtocol!
     private (set) var restartGameButtonIsHidden: Bool! = true
-    
+    private (set) var gameLabelText: String = "Player X turn"
+
     init(game: GameProtocol) {
         self.game = game
     }
