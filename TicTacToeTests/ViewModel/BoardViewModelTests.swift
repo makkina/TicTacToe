f93bbe84ffe9cd99fd65a9694e98b691db340231
makkina <efe@appamist.com>
2021-07-11 19:23:39 +0200

    test_canCreateBoardViewModel

---
+++ TicTacToeTests/ViewModel/BoardViewModelTests.swift
@@ -0,0 +1,13 @@
+// Created 11/07/2021
+
+import XCTest
+@testable import TicTacToe
+
+class BoardViewModelTests: XCTestCase {
+    
+    func test_canCreateBoardViewModel() {
+        
+        let viewModel = BoardViewModel()
+        
+    }
+}
