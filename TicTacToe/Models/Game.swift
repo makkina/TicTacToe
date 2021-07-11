dc4dc47b02a0d235ab4a3f6310f1f0d431618e7c
makkina <efe@appamist.com>
2021-07-11 12:25:54 +0200

    test_canCreateGame

---
+++ TicTacToe/Models/Game.swift
@@ -0,0 +1,7 @@
+// Created 11/07/2021
+
+import Foundation
+
+struct Game {
+    
+}
---
+++ TicTacToeTests/GameTests.swift
@@ -0,0 +1,11 @@
+// Created 11/07/2021
+
+import XCTest
+@testable import TicTacToe
+
+class GameTests: XCTestCase {
+
+    func test_canCreateGame() {
+        let game = Game()
+    }
+}
