0c74c00592df150ef8e498777ca3e31adf3c4e3b
makkina <efe@appamist.com>
2021-07-11 07:28:58 +0200

    Xcode draft project

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,14 @@
+// Created 11/07/2021
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
