49121f5c2f188faf5431114fd37c5194e920b3c0
makkina <efe@appamist.com>
2021-07-12 15:23:58 +0200

    Create View in Storyboard and setup Outlets and Ibactions

--- TicTacToe/ViewControllers/BoardViewController.swift
+++ TicTacToe/ViewControllers/BoardViewController.swift
@@ -3,9 +3,22 @@
 import UIKit
 
 class BoardViewController: UIViewController {
+    
+    @IBOutlet weak var gameLabel: UILabel!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var resetGameButton: UIButton!
+    
+    @IBOutlet var allBoardButtons: [BoardButton]!
+    
+    @IBAction func resetGameButtonPressed(_ sender: Any) {
+        
+    }
 
+    // MARK: - Setup
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
