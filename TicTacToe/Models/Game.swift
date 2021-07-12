e6d3e64f1e75f7005d51dd937e92c6156dc7f33b
makkina <efe@appamist.com>
2021-07-12 16:15:09 +0200

    first error to check is now gameIsOverError

--- TicTacToe/Models/Game.swift
+++ TicTacToe/Models/Game.swift
@@ -90,14 +90,14 @@ private extension Game {
     }
     
     func gameErrorFound(_ index: Int) -> GameError? {
+        guard !isGameOver() else {
+            return GameError.gameIsOverError
+        }
         guard [0, 1, 2, 3, 4, 5, 6, 7, 8].contains(index) else {
             return GameError.indexOutOfRangeError
         }
         guard selectedBoardValues[index] == 0 else {
             return GameError.indexAlreadyPopulatedError
-        }
-        guard !isGameOver() else {
-            return GameError.gameIsOverError
         }
         return nil
     }
