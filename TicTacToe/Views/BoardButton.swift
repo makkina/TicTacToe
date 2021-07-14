d1544c8a157c2688fddc174253e79c99da03ba15
makkina <efe@appamist.com>
2021-07-14 20:32:32 +0200

    Remove redundant string rawvalue declaration in SymbolName Enum

--- TicTacToe/Views/BoardButton.swift
+++ TicTacToe/Views/BoardButton.swift
@@ -3,15 +3,19 @@
 import UIKit
 
 enum SymbolName: String {
-    case Xmark = "xmark"
-    case Circle = "circle"
+    case xmark
+    case circle
+    
+    var image: UIImage? {
+        UIImage.withSymbolName(rawValue)
+    }
 }
 
 final class BoardButton: UIButton {
 
     var symbolName: SymbolName? {
         didSet {
-            setImage(UIImage.withSymbolName(symbolName?.rawValue ?? ""), for: .normal)
+            setImage(symbolName?.image, for: .normal)
         }
     }
     
