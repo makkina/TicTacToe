72be6d0426345e5d574353702b830f42159010cf
makkina <efe@appamist.com>
2021-07-15 13:45:19 +0200

    Move allBoardButton creation to setup

--- TicTacToeUITests/BoardUITests.swift
+++ TicTacToeUITests/BoardUITests.swift
@@ -5,25 +5,30 @@ import XCTest
 final class BoardUITests: XCTestCase {
     
     private var app: XCUIApplication!
+    private var allBoardButtons: [XCUIElement]!
     
     override func setUp() {
         super.setUp()
         app = XCUIApplication()
         app.launch()
+        
+        allBoardButtons = [XCUIElement]()
+        for i in 1...9 {
+            allBoardButtons.append(app.buttons["boardButton \(i)"])
+        }
     }
     
     func test_viewHas9boardButtons() {
-        for i in 1...9 {
-            let button = app.buttons["boardButton \(i)"]
+        for button in allBoardButtons {
             XCTAssertTrue(button.waitForExistence(timeout: 1))
         }
     }
     
     func test_givenGameWasWon_resetGameButtonBecomesHittable() {
-        for i in 1...9 {
-            app.buttons["boardButton \(i)"].tap()
+        for button in allBoardButtons {
+            button.tap()
         }
-
+        
         let resetGameButton = app.buttons["resetGameButtonIdentifier"]
         XCTAssertTrue(resetGameButton.isHittable)
     }
