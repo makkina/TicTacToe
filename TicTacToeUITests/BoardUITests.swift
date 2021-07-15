cbf093a3ffbacef5b002c5ba01006b0c91da3e4a
makkina <efe@appamist.com>
2021-07-15 14:40:20 +0200

    test_givenGameWasDraw_resetGameButtonBecomesHittable

--- TicTacToeUITests/BoardUITests.swift
+++ TicTacToeUITests/BoardUITests.swift
@@ -21,9 +21,35 @@ final class BoardUITests: XCTestCase {
         }
     }
     
+    // MARK: - Game is Over
+    
+    /*
+     Board Composition:
+        x o x
+        o x o
+        x - -
+     */
     func test_givenGameWasWon_resetGameButtonBecomesHittable() {
         for button in allBoardButtons {
             button.tap()
+        }
+        
+        let resetGameButton = app.buttons["resetGameButtonIdentifier"]
+        XCTAssertTrue(resetGameButton.isHittable)
+    }
+
+    /*
+     Board Composition:
+        1=x   2=o   3=x
+        4=x   5=o   6=x
+        7=o   8=x   9=o
+     */
+    func test_givenGameWasDraw_resetGameButtonBecomesHittable() {
+                         // x  o  x  o  x  o  x  o  x
+        let drawSequence = [1, 2, 3, 5, 6, 7, 8, 9, 4]
+        
+        for index in drawSequence {
+            allBoardButtons[index-1].tap()
         }
         
         let resetGameButton = app.buttons["resetGameButtonIdentifier"]
